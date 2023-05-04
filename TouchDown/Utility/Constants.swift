88f4188f2d01a43454449f2d68b0f10e2e34ddf2
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-05-04 23:33:24 +0200

    Add Icon and launch Screen

---
+++ TouchDown/Utility/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  TouchDown
+//
+//  Created by Teenu Abraham on 04/05/23.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
